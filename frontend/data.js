bad038526e3e729e0040b40360031d52711fe0ef
Henri Lemoine <henri123lemoine@gmail.com>
2025-03-14 15:29:09 -0400

    Update data a bit and add some todos to readme

--- frontend/data.js
+++ frontend/data.js
@@ -5,12 +5,12 @@ const modelData = [
   { name: "Sonnet 3.6", score: 165 },
   // { name: "Sonnet 3.5", score:  },
   // { name: "Opus 3", score: refusal },
-  { name: "Grok 3\n(inc. thinking)", score: 225 },
+  { name: "Grok 3\n(thinking)", score: 225 },
   { name: "GPT-4o", score: 315 },
   { name: "GPT-4.5", score: 225 },
   { name: "o3-mini\n(high)", score: 300 },
   { name: "Deepseek\nR1", score: 225 },
   { name: "Gemini 2.0\nFlash\nExperimental", score: 500 },
-  { name: "Gemini 2.0\nFlash\nLite", score: 315 },
-  { name: "Llama 3.1 405B", score: 315 }
+  { name: "Gemini 2.0\nFlash-lite", score: 315 },
+  { name: "Llama 3.1\n(405B)", score: 315 }
 ];
